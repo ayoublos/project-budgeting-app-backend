fc80c0a9589b9bb65ccab680726a6edda3e3bc1f
ayoublos <abelibel@pursuit.org>
2024-07-08 20:11:21 -0700

    updated model

--- models/transaction.js
+++ models/transaction.js
@@ -4,7 +4,7 @@ module.exports=
         id: 1,
         item_name: "Salary",
         amount: 5000,
-        date: "2024-07-06",
+        date: "7/6/2024",
         from: "Employer",
         category: "Income"
     },
@@ -12,7 +12,7 @@ module.exports=
         id: 2,
         item_name: "Groceries",
         amount: -75.5,
-        date: "2024-07-05",
+        date: "4/8/2024",
         from: "Grocery Store",
         category: "Food"
     },
@@ -20,7 +20,7 @@ module.exports=
         id: 3,
         item_name: "Savings Deposit",
         amount: 1000,
-        date: "2024-07-04",
+        date: "7/3/2024",
         from: "Bank",
         category: "Savings"
     },
@@ -28,7 +28,7 @@ module.exports=
         id: 4,
         item_name: "Cat Food",
         amount: -20,
-        date: "2024-07-03",
+        date: "6/4/2024",
         from: "Pet Store",
         category: "Pets"
     }
